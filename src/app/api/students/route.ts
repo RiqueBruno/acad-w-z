6ce0d742997fc6429cfe37e62b93c147fc3dedc1
Bruno H <riquebruno@yahoo.com.br>
2024-11-22 15:39:47 -0300

    feat: students/id - adiciona rota para adicionar novo estudante

--- src/app/api/students/route.ts
+++ src/app/api/students/route.ts
@@ -10,6 +10,12 @@ const serviceAccountAuth = new JWT({
   scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
 });
 
+const serviceAccountAuthEdit = new JWT({
+  email: process.env.GOOGLE_CLIENT_EMAIL,
+  key: process.env.GOOGLE_PRIVATE_KEY,
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+});
+
 export async function GET() {
   const doc = new GoogleSpreadsheet(
     '1QPi8_YOSghYt0i-nhDOIfq9DZvBgcQqEe-tioetSZYg',
@@ -21,6 +27,7 @@ export async function GET() {
   try {
     const sheet = doc.sheetsByIndex[0];
     const data = await sheet.getRows();
+
     const rows: Student[] = data.map((row) => {
       return {
         id: row.get('id') || '',
@@ -51,5 +58,74 @@ export async function GET() {
   } catch (err) {
     console.error(err);
     return Response.json({ error: err });
+  }
+}
+
+export async function POST(request: Request) {
+  const doc = new GoogleSpreadsheet(
+    '1QPi8_YOSghYt0i-nhDOIfq9DZvBgcQqEe-tioetSZYg',
+    serviceAccountAuthEdit
+  );
+
+  await doc.loadInfo();
+
+  try {
+    const sheet = doc.sheetsByIndex[0];
+    const student: Promise<Student> = request.json();
+
+    if (!student) {
+      return Response.json({ error: 'Invalid data' }, { status: 400 });
+    }
+
+    if (
+      !(await student).id ||
+      !(await student).name ||
+      !(await student).birthDate ||
+      !(await student).cpf
+    ) {
+      return Response.json(
+        { error: 'Missing required fields' },
+        { status: 400 }
+      );
+    }
+
+    const newStudent = {
+      id: (await student).id,
+      name: (await student).name,
+      birthDate: (await student).birthDate,
+      gender: (await student).gender,
+      maritalStatus: (await student).maritalStatus,
+      profession: (await student).profession,
+      cpf: (await student).cpf,
+      rg: (await student).rg,
+      issuingAgency: (await student).issuingAgency,
+      postalCode: (await student).postalCode,
+      neighborhood: (await student).neighborhood,
+      address: (await student).address,
+      number: (await student).number,
+      complement: (await student).complement,
+      city: (await student).city,
+      state: (await student).state,
+      placeOfBirth: (await student).placeOfBirth,
+      nationality: (await student).nationality,
+      TEL1: (await student).phone[0],
+      TEL2: (await student).phone[1],
+      EMAIL1: (await student).email[0],
+      EMAIL2: (await student).email[1],
+      INSTAGRAM1: (await student).instagram[0],
+      INSTAGRAM2: (await student).instagram[1],
+    };
+
+    await sheet.addRow(newStudent);
+
+    return Response.json(
+      {
+        message: 'Student added successfully',
+        student: newStudent,
+      },
+      { status: 201 }
+    );
+  } catch (err) {
+    console.error(err);
   }
 }
