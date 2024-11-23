769849ca39abe2678b3a700feeb63f75620b716d
Bruno H <riquebruno@yahoo.com.br>
2024-11-23 20:00:06 -0300

    feat: NavItem - cria componente para gerar <li>  de navegação.

---
+++ src/components/NavItem/index.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+type NavItemProps = {
+  text: string;
+  url: string;
+  cssLi: string;
+  cssLink: string;
+};
+
+export default function NavItem({ text, url, cssLi, cssLink }: NavItemProps) {
+  return (
+    <li className={cssLi}>
+      <Link href={url} className={cssLink}>
+        {text}
+      </Link>
+    </li>
+  );
+}
