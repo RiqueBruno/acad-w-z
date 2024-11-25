2835510cd38e2e1c9eea6503da4d78adea7cc044
Bruno H <riquebruno@yahoo.com.br>
2024-11-24 22:32:32 -0300

    refactor: Header - melhora o codigo adicionando map com o componente NavItem e estiliza deixando responsivo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,33 +1,76 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import NavItem from '../NavItem';
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navNames = ['Home', 'Sobre', 'Cursos', 'Serviços', 'Contato'];
+
   return (
-    <header>
-      <Image
-        src="/logo.svg"
-        alt="logo Academia Waleska Zanyor"
-        width={100}
-        height={100}
-      />
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/sobre">Sobre</Link>
-        </li>
-        <li>
-          <Link href="/cursos">Cursos</Link>
-        </li>
-        <li>
-          <Link href="/servicos">Serviços</Link>
-        </li>
-        <li>
-          <Link href="/contato">Contato</Link>
-        </li>
-      </ul>
+    <header className="bg-white shadow-md z-[99] fixed w-full">
+      <div className="container mx-auto flex justify-between items-center py-4 px-6">
+        <Link href="/">
+          <Image
+            src="/logo.webp"
+            alt="Logo Academia Waleska Zanyor"
+            width={200}
+            height={200}
+            className="cursor-pointer"
+          />
+        </Link>
+
+        <button
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className="block md:hidden p-2 text-gray-800 focus:outline-none"
+          aria-label="Toggle menu"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            {isMenuOpen ? (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            ) : (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            )}
+          </svg>
+        </button>
+
+        <nav
+          className={`${
+            isMenuOpen
+              ? 'absolute top-16 right-6 bg-white w-48 rounded-md shadow-md transition-all duration-300'
+              : 'hidden md:block'
+          } md:space-x-6 md:flex md:flex-row md:items-center md:justify-end`}
+        >
+          <ul className="py-2 md:py-0 md:flex md:items-center md:justify-center">
+            {navNames.map((name) => (
+              <NavItem
+                key={name}
+                text={name}
+                url={name === 'Home' ? '/' : `/${name.toLowerCase()}`}
+                cssLink="block px-4 py-2 text-gray-800 hover:bg-gray-200 md:hover:text-gray-500 md:text-gray-800"
+                cssLi=""
+              />
+            ))}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
