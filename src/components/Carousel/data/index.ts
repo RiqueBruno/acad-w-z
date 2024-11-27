54cf5f203bf735eee9eb9e15ebb2c5da31fcc687
Bruno H <riquebruno@yahoo.com.br>
2024-11-27 10:48:31 -0300

    feat: Carousel - cria um data para o Carousel

---
+++ src/components/Carousel/data/index.ts
@@ -0,0 +1,31 @@
+import { CarouselItem } from '..';
+
+const carouselData: CarouselItem[] = [
+  {
+    image: '/acaoSocial.jpg',
+    title: 'Curso de Cabeleireiro',
+    subtext: 'Aprenda a cortar cabelos e a fazer penteados incríveis.',
+    buttonText: 'Inscreva-se',
+    layout: 3,
+    link: '',
+  },
+  {
+    image: '/acaoSocial.jpg',
+    title: 'Ação social - Obra do Berço - 2024',
+    subtext:
+      'Ação social realizada na instituição Obra do Berço, localizada em Rio de Janeiro.',
+    buttonText: 'Leia mais',
+    layout: 1,
+    link: '',
+  },
+  {
+    image: '/acaoSocial.jpg',
+    title: 'Seja Modelo',
+    subtext: 'Torne-se um modelo e torne seus desejos realidade.',
+    buttonText: 'Ser Modelo',
+    layout: 2,
+    link: '',
+  },
+];
+
+export default carouselData;
