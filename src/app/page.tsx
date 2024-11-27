fe506079748b0f6ff5c3cc533f531af0bfd6629b
Bruno H <riquebruno@yahoo.com.br>
2024-11-27 10:49:30 -0300

    refactor: page - adiciona componente Carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,12 @@
 'use client';
 
-export default function Home() {
-  const fetchData = async () => {
-    try {
-      const response = await fetch('/api/studants');
-      if (!response.ok) {
-        throw new Error('Erro ao buscar dados');
-      }
-      const data = await response.json();
-      console.log('Dados obtidos com sucesso:', data);
-    } catch (error) {
-      console.error('Erro ao obter dados:', error);
-    }
-  };
+import CarouselComponent from '@/components/Carousel';
+import carouselData from '@/components/Carousel/data';
 
+export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <button onClick={fetchData}>Teste</button>
+    <div className="pt-20">
+      <CarouselComponent carouselItems={carouselData} />
     </div>
   );
 }
