9ed3ca2c243f891b5d3d7c2c52fa4724dae1ae94
Bruno H <riquebruno@yahoo.com.br>
2024-11-26 23:21:27 -0300

    refactor: tailwind config - atualiza palheta de cores

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        colors: {
+          brand: {
+            primary: '#FFFFFF',
+            secondary: '#4C4B4B',
+            tertiary: '#9E624D',
+            black: '#000000',
+          },
+          gradient: {
+            start: '#9E624D',
+            mid: '#D0A692',
+            end: '#9E624D',
+          },
+        },
       },
     },
   },
