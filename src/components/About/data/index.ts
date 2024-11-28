cbbf3e56aca25700fdf9cd8e981d2386d66b8113
Bruno H <riquebruno@yahoo.com.br>
2024-11-28 20:19:41 -0300

    feat: About - cria data para organizar as informações

---
+++ src/components/About/data/index.ts
@@ -0,0 +1,15 @@
+export interface AboutInfos {
+  title: string;
+  text: string;
+  image: string;
+  alt: string;
+}
+
+const aboutInfos: AboutInfos = {
+  title: 'Sobre a Academia',
+  text: 'Fundada por Waleska Zanyor, a Academia oferece diversos cursos e serviços para todos. Apesar de tão pouco tempo de vida, já formamos muitos profissionais. A Academia Waleska Zanyor oferece cursos de formação única no mercado de coiffure, preparando para o mercado profissionais completos, treinados nas diversas funções dentro de um salão...',
+  image: '/BG.png',
+  alt: 'Imagem de Waleska Zanyor',
+};
+
+export default aboutInfos;
