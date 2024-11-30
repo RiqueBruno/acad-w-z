49a31c9b276b4d348ba84a1fab70ad5d2535de28
Bruno H <riquebruno@yahoo.com.br>
2024-11-30 11:00:00 -0300

    feat: SocialAction - adiciona link para página sobre ações sociais e atualiza estilo do componente

--- src/components/SocialAction/index.tsx
+++ src/components/SocialAction/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import CarouselComponent, { CarouselItem } from '../Carousel';
 
 type SocialActionProps = {
@@ -6,12 +7,18 @@ type SocialActionProps = {
 
 export default function SocialAction({ arr }: SocialActionProps) {
   return (
-    <section className="bg-gray-100 py-12">
-      <div className="container mx-auto">
-        <h2 className="text-2xl text-center font-bold text-gray-800">
+    <section className="py-12 bg-colors-brand-primary mt-16">
+      <div className="container mx-auto space-y-24 px-16 flex flex-col items-center justify-center">
+        <h2 className="text-3xl lg:text-4xl text-center font-bold text-gray-800">
           Ações sociais
         </h2>
         <CarouselComponent carouselItems={arr} />
+        <Link
+          href="/sobre"
+          className="text-xl text-center text-colors-brand-primary bg-colors-brand-tertiary rounded-md p-4 mt-4"
+        >
+          Saiba mais sobre nossas ações sociais
+        </Link>
       </div>
     </section>
   );
