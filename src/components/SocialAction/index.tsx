d2e5a18d8b7dabf1181003cbef7fa7ccd7e87a09
Bruno H <riquebruno@yahoo.com.br>
2024-11-30 11:02:19 -0300

    feat: SocialAction - adiciona tipo 'social' ao componente Carousel

--- src/components/SocialAction/index.tsx
+++ src/components/SocialAction/index.tsx
@@ -8,11 +8,11 @@ type SocialActionProps = {
 export default function SocialAction({ arr }: SocialActionProps) {
   return (
     <section className="py-12 bg-colors-brand-primary mt-16">
-      <div className="container mx-auto space-y-24 px-16 flex flex-col items-center justify-center">
+      <div className="container mx-auto space-y-16 px-16 flex flex-col items-center justify-center">
         <h2 className="text-3xl lg:text-4xl text-center font-bold text-gray-800">
           Ações sociais
         </h2>
-        <CarouselComponent carouselItems={arr} />
+        <CarouselComponent carouselItems={arr} type="social" />
         <Link
           href="/sobre"
           className="text-xl text-center text-colors-brand-primary bg-colors-brand-tertiary rounded-md p-4 mt-4"
