6edd51b40f9b2843369fe9ef8d55a64e6ef0c039
Bruno H <riquebruno@yahoo.com.br>
2024-11-30 10:38:32 -0300

    feat: SocialAction - cria o data para adicionar dados

---
+++ src/components/SocialAction/data/index.ts
@@ -0,0 +1,38 @@
+import { CarouselItem } from '../../Carousel/index';
+
+const socialData: CarouselItem[] = [
+  {
+    image: '/acaoSocial.jpg',
+    title: 'Ação social - Obra do Berço - 2024',
+    subtext: '',
+    buttonText: '',
+    layout: 4,
+    link: '',
+  },
+  {
+    image: '/acaoSocial.jpg',
+    title: 'Ação social - Obra do Berço - 2024',
+    subtext: '',
+    buttonText: '',
+    layout: 4,
+    link: '',
+  },
+  {
+    image: '/acaoSocial.jpg',
+    title: 'Ação social - Obra do Berço - 2024',
+    subtext: '',
+    buttonText: '',
+    layout: 4,
+    link: '',
+  },
+  {
+    image: '/acaoSocial.jpg',
+    title: 'Ação social - Obra do Berço - 2024',
+    subtext: '',
+    buttonText: '',
+    layout: 4,
+    link: '',
+  },
+];
+
+export default socialData;
