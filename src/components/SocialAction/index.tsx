7ebb9d928e2d2d3c8c7cd3d4021da4a23be2d974
Bruno H <riquebruno@yahoo.com.br>
2024-11-30 11:34:24 -0300

    feat: SocialAction component - adiciona descrição e melhora a estrutura do cabeçalho

--- src/components/SocialAction/index.tsx
+++ src/components/SocialAction/index.tsx
@@ -8,17 +8,23 @@ type SocialActionProps = {
 export default function SocialAction({ arr }: SocialActionProps) {
   return (
     <section className="py-12 bg-colors-brand-primary mt-16">
-      <div className="container mx-auto space-y-16 px-16 flex flex-col items-center justify-center">
-        <h2 className="text-3xl lg:text-4xl text-center font-bold text-gray-800">
-          Ações sociais
-        </h2>
+      <div className="container mx-auto space-y-16 md:px-16 flex flex-col items-center justify-center">
+        <header className="w-full space-y-4">
+          <h2 className="text-3xl lg:text-4xl text-center font-bold text-gray-800">
+            Ações sociais
+          </h2>
+          <p className="w-full text-center">
+            A Academia Waleska Zanyor se preocupa com a comunidade e realiza
+            diversas ações sociais. Confira algumas delas:
+          </p>
+        </header>
         <CarouselComponent
           carouselItems={arr}
           type="Ações sociais feitas pela Academia Waleska Zanyor"
         />
         <Link
           href="/sobre"
-          className="text-xl text-center text-colors-brand-primary bg-colors-brand-tertiary rounded-md p-4 mt-4"
+          className="text-xl text-center text-colors-brand-primary bg-colors-brand-tertiary rounded-md p-4 mt-4 hover:opacity-90"
         >
           Saiba mais sobre nossas ações sociais
         </Link>
