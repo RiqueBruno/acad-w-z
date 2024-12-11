402e8570e14c8aaa4c023e468105dbaaf0c16b87
Bruno H <riquebruno@yahoo.com.br>
2024-12-11 16:58:03 -0300

    feat: sobre page - adiciona novo componente e estrutura para a página sobre

---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,20 @@
+import Ghost from '@/components/Ghost';
+
+export default function Sobre() {
+  return (
+    <main id="main-title">
+      <Ghost />
+      <h2 id="main-title" className="sr-only">
+        Pagina sobre a Academia Waleska Zanyor e suas ações sociais.
+      </h2>
+      <section>
+        <h2>Academia Waleska Zanyor</h2>
+        <p>texto</p>
+      </section>
+      <section>
+        <h2>Header</h2>
+        <p>Header</p>
+      </section>
+    </main>
+  );
+}
