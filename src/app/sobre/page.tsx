8de64202333ae7526811cf92f41b07597f2ff183
Bruno H <riquebruno@yahoo.com.br>
2024-12-11 17:12:43 -0300

    feat: sobre page - adiciona imagem da Academia Waleska Zanyor

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -8,6 +8,14 @@ export default function Sobre() {
         Pagina sobre a Academia Waleska Zanyor e suas ações sociais.
       </h2>
       <section>
+        <div>
+          <Image
+            src="/images/academia-waleska-zanyor.jpg"
+            alt="Academia Waleska Zanyor"
+            width={400}
+            height={400}
+          />
+        </div>
         <h2>Academia Waleska Zanyor</h2>
         <p>texto</p>
       </section>
