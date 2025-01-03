dab415d03ddd8febd470ad62cc5ec25183196e53
Bruno H <riquebruno@yahoo.com.br>
2025-01-03 16:33:51 -0300

    feat: footer - add data structure for footer links

---
+++ src/components/Footer/data/index.ts
@@ -0,0 +1,18 @@
+export const data = [
+  {
+    title: 'Sobre Nós',
+    link: 'sobre',
+  },
+  {
+    title: 'Trabalhe Conosco',
+    link: 'work',
+  },
+  {
+    title: 'Torne-se um parceiro',
+    link: 'partner',
+  },
+  {
+    title: 'Dúvidas',
+    link: 'faq',
+  },
+];
