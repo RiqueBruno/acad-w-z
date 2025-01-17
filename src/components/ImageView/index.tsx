b9824ce1cf4121d394eec5bef9de362d4ee7e0f1
Bruno H <riquebruno@yahoo.com.br>
2025-01-17 11:12:56 -0300

    feat: imageView - cria componente

---
+++ src/components/ImageView/index.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import { useState } from 'react';
+import Image from 'next/image';
+import { MdOutlineZoomIn } from 'react-icons/md';
+
+interface ImageViewerProps {
+  src: string;
+  alt: string;
+}
+
+const ImageViewer: React.FC<ImageViewerProps> = ({ src, alt }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  return (
+    <div className="relative group">
+      <Image
+        src={src}
+        alt={alt}
+        width={400}
+        height={400}
+        className="object-cover rounded-lg"
+      />
+      <div className="absolute inset-0 group-hover:opacity-0 flex items-start justify-end text-white text-sm md:hidden font-semibold transition-opacity duration-300 rounded-lg hover:bg-black/50">
+        <MdOutlineZoomIn className="text-4xl" />
+      </div>
+      <button
+        onClick={toggleModal}
+        className="absolute hidden inset-0 bg-black/50 opacity-0 group-hover:opacity-100 md:flex items-center justify-center text-white text-sm md:text-4xl font-semibold transition-opacity duration-300 rounded-lg"
+      >
+        <MdOutlineZoomIn />
+      </button>
+
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-black/80 z-50 flex items-center justify-center mt-10">
+          <div className="relative">
+            <Image
+              src={src}
+              alt={alt}
+              width={800}
+              height={800}
+              className="rounded-lg"
+            />
+            <button
+              onClick={toggleModal}
+              className="absolute top-2 right-2 text-white bg-red-500 hover:bg-red-600 rounded-lg p-2"
+            >
+              ✕
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ImageViewer;
