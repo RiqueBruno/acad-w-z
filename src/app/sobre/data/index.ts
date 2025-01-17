124544f00f3620be63b3f21f8b7bda6d5a576e1c
Bruno H <riquebruno@yahoo.com.br>
2025-01-17 10:54:34 -0300

    feat: data sobre - cria array com dados das fotos das ações sociais

---
+++ src/app/sobre/data/index.ts
@@ -0,0 +1,27 @@
+type imageViewer = {
+  src: string;
+  alt: string;
+};
+
+export const imageViewer: imageViewer[] = [
+  {
+    src: '/acaoSocial.jpg',
+    alt: 'Ação social 3',
+  },
+  {
+    src: '/acaoSocial.jpg',
+    alt: 'Ação social 2',
+  },
+  {
+    src: '/acaoSocial.jpg',
+    alt: 'Ação social 1',
+  },
+  {
+    src: '/acaoSocial.jpg',
+    alt: 'Ação social 1',
+  },
+  {
+    src: '/acaoSocial.jpg',
+    alt: 'Ação social 1',
+  },
+];
