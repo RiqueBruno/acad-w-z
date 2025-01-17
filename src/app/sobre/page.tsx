eb4b0a1bacfdc7199d244887699e781a4a5dbec0
Bruno H <riquebruno@yahoo.com.br>
2025-01-17 11:49:27 -0300

    refactor: sobre - ajusta espaçamento e centraliza texto nas seções

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -5,12 +5,12 @@ import { imagesViewer } from './data';
 
 export default function Sobre() {
   return (
-    <main id="main-title" className="px-16 lg:px-32 py-8">
+    <main id="main-title" className="px-8 md:px-16 lg:px-32 py-8">
       <Ghost />
       <h2 id="main-title" className="sr-only">
-        Pagina sobre a Academia Waleska Zanyor e suas ações sociais.
+        Sobre a Academia Waleska Zanyor e suas ações sociais.
       </h2>
-      <section className="flex flex-col items-center bg-colors-brand-primary text-colors-brand-secondary p-16 shadow-lg rounded-lg">
+      <section className="flex flex-col items-center bg-colors-brand-primary text-colors-brand-secondary p-8 md:p-16 shadow-lg rounded-lg text-center">
         <div>
           <Image
             src="/images/academia-waleska-zanyor.jpg"
@@ -36,7 +36,7 @@ export default function Sobre() {
           de instrutores.
         </p>
       </section>
-      <section className="flex flex-col items-center bg-colors-brand-primary text-colors-brand-secondary p-16 shadow-lg rounded-lg mt-8">
+      <section className="flex flex-col items-center bg-colors-brand-primary text-colors-brand-secondary p-16 shadow-lg rounded-lg mt-8 text-center">
         <h2 className="text-3xl lg:text-4xl font-bold text-gray-800 mb-4">
           Waleska Zanyor
         </h2>
@@ -50,7 +50,7 @@ export default function Sobre() {
           <p>Sobre</p>
         </div>
       </section>
-      <section className="flex flex-col items-center bg-colors-brand-primary text-colors-brand-secondary p-16 shadow-lg rounded-lg mt-8">
+      <section className="flex flex-col items-center bg-colors-brand-primary text-colors-brand-secondary p-16 shadow-lg rounded-lg mt-8 text-center">
         <h2 className="text-3xl lg:text-4xl font-bold text-gray-800 mb-4">
           Ações sociais
         </h2>
@@ -63,10 +63,10 @@ export default function Sobre() {
           vidas e queremos ajudar a mudar a vida de quem mais precisa.
         </p>
         <article className="flex flex-col items-center mt-8 border-t border-gray-300 pt-8">
-          <h3 className="text-3xl lg:text-1xl font-bold text-gray-800 mb-4">
+          <h3 className="text-2xl lg:text-3xl font-bold text-gray-800 mb-4">
             Fotos
           </h3>
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-h-[64rem] p-2 overflow-y-auto rounded-lg">
             {imagesViewer.map((image, index) => (
               <ImageViewer key={index} src={image.src} alt={image.alt} />
             ))}
