2796f0250c4e6a57b76f992fb9858d32c2aa9ded
Bruno H Cardoso <riquebruno@yahoo.com.br>
2025-03-03 11:35:58 -0300

    refactor: page.tsx app - muda estilização retirando pt de main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ import socialData from '@/components/SocialAction/data';
 
 export default function Home() {
   return (
-    <main className="pt-20" aria-labelledby="main-title">
+    <main aria-labelledby="main-title">
       <h2 id="main-title" className="sr-only">
         Pagina principal do site Academia Waleska Zanyor
       </h2>
