896c520653eebe8dd09637ce921024a19dbc80c3
Bruno Henrique <riquebruno@yahoo.com.br>
2026-02-21 17:42:47 -0300

    Refactor Home component: improve structure, enhance accessibility, and update headings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,45 @@
-'use client';
-
-import About from '@/components/About';
-import BeModelAd from '@/components/BeModelAd';
-import CarouselComponent from '@/components/Carousel';
-import carouselData from '@/components/Carousel/data';
-import CoursesAd from '@/components/CoursesAd';
-import CoursesCarousel from '@/components/CoursesCarousel';
-import courses from '@/components/CoursesCarousel/data';
-import Ghost from '@/components/Ghost';
-import ServiceList from '@/components/ServiceList';
-import services from '@/components/ServiceList/data';
-import SocialAction from '@/components/SocialAction';
-import socialData from '@/components/SocialAction/data';
+import About from "@/components/About";
+import BeModelAd from "@/components/BeModelAd";
+import CarouselComponent from "@/components/Carousel";
+import carouselData from "@/components/Carousel/data";
+import CoursesAd from "@/components/CoursesAd";
+import CoursesCarousel from "@/components/CoursesCarousel";
+import courses from "@/components/CoursesCarousel/data";
+import Ghost from "@/components/Ghost";
+import ServiceList from "@/components/ServiceList";
+import services from "@/components/ServiceList/data";
+import SocialAction from "@/components/SocialAction";
+import socialData from "@/components/SocialAction/data";
 
 export default function Home() {
   return (
-    <main aria-labelledby="main-title">
-      <h2 id="main-title" className="sr-only">
-        Pagina principal do site Academia Waleska Zanyor
-      </h2>
+    <main>
+      <h1 className="sr-only">
+        Academia Waleska Zanyor - Formação Profissional para Cabeleireiros
+      </h1>
+
       <Ghost />
       <CarouselComponent carouselItems={carouselData} />
-      <About />
-      <ServiceList services={services} />
+
+      <section id="sobre">
+        <About />
+      </section>
+
+      <section id="servicos">
+        <ServiceList services={services} />
+      </section>
+
       <CoursesAd />
-      <CoursesCarousel courses={courses} />
+
+      <section id="cursos">
+        <CoursesCarousel courses={courses} />
+      </section>
+
       <BeModelAd />
-      <SocialAction arr={socialData} />
+
+      <section id="acoes-sociais">
+        <SocialAction arr={socialData} />
+      </section>
     </main>
   );
 }
