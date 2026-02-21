f4a41b781e5e75ddc92e9bacee5363ff48e61ec7
Bruno Henrique <riquebruno@yahoo.com.br>
2026-02-21 19:46:17 -0300

    feat: add cabeleireiro image

--- src/components/CoursesCarousel/data/index.ts
+++ src/components/CoursesCarousel/data/index.ts
@@ -5,12 +5,12 @@ export type Course = {
 };
 
 const courses: Course[] = [
-  { id: 1, name: 'Curso de Cabeleireiro', image: '/cabeleireiro.jpg' },
-  { id: 2, name: 'Curso de Coloração', image: '/coloracao.jpg' },
-  { id: 3, name: 'Curso de Corte', image: '/corte.jpg' },
-  { id: 4, name: 'Curso de Escova', image: '/escova.jpg' },
-  { id: 5, name: 'Curso de Corte Avançado', image: '/corte.jpg' },
-  { id: 6, name: 'Curso de Express de Escova', image: '/escova.jpg' },
+  { id: 1, name: "Curso de Cabeleireiro", image: "/cabeleireiro.png" },
+  { id: 2, name: "Curso de Coloração", image: "/coloracao.jpg" },
+  { id: 3, name: "Curso de Corte", image: "/corte.jpg" },
+  { id: 4, name: "Curso de Escova", image: "/escova.jpg" },
+  { id: 5, name: "Curso de Corte Avançado", image: "/corte.jpg" },
+  { id: 6, name: "Curso de Express de Escova", image: "/escova.jpg" },
 ];
 
 export default courses;
